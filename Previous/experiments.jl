50e06dcd55330533d4e0410a1e9f911bb27bde76
Raul Garcia <rauljgarcia66@gmail.com>
2023-03-07 22:44:26 -0600

    Upd

--- Previous/experiments.jl
+++ Previous/experiments.jl
@@ -67,7 +67,7 @@ end
 ###################################################################################
 ################################ Interesting Cases ################################
 #### Sep 20
-A= [0 -5 -2 -4 -10; 5 -6 4 7 2; 2 -4 5 0 -8; 4 -7 0 -9 7; 10 -2 8 -7 0]
+A = [0 -5 -2 -4 -10; 5 -6 4 7 2; 2 -4 5 0 -8; 4 -7 0 -9 7; 10 -2 8 -7 0]
 # Interesting case. s has two 1's and x his a singleton.
 A_12 = A * 12
 
@@ -81,20 +81,26 @@ A = [0 -4 -9 0 6; 4 0 7 -3 4; 9 -7 0 4 -8; 0 3 -4 0 10; -6 -4 8 -10 0]
 # Diagonal is 0s.
 # OBSERVATION: A fair game occurs when the matrix is negative symmetric (diagonals do not have to be 0)
 # and all plays are available to the players.
-
 A = [0 0 0 3 -1; 0 0 -6 -3 0; 0 6 0 8 -9; -3 3 -8 0 -6; 1 0 9 6 0]
-
 A = [0 1 -1 1 -1; -1 0 1 -1 1; 1 -1 0 1 -1; -1 1 -1 0 1; 1 -1 1 -1 0]
+
+#### Oct 7
+# Must play equal number of rows and columns
+A = [4 -9 9 -5 -3; 9 -3 3 0 2; -9 -3 3 -6 2; 5 0 6 6 8; 3 -2 -2 -8 -5]
+A = [0 -5 -2 -4 -10; 5 -6 4 7 2; 2 -4 5 0 -8; 4 -7 0 -9 7; 10 -2 8 -7 0]
+A = [0 0 0 3 -1; 0 0 -6 -3 0; 0 6 0 8 -9; -3 3 -8 0 -6; 1 0 9 6 0]
+A = [0 1 -1 1 -1; -1 0 1 -1 1; 1 -1 0 1 -1; -1 1 -1 0 1; 1 -1 1 -1 0]
+# Interesting. Optimal solution x is always play all plays an equal amount (ie, x = ones(m) / m ).
+# Value of the game is always 0.
 
 # Try different cost vectors
 # Try constrains on plays. Rows eliminated >= or == or <= columns eliminated. (Seemingly fair)
 # Look at nonsymmetric matrices and nonsquare matrices
 
-
 ###################################################################################
 ###################################################################################
 # Scale matrix by a range of values to find balance with other parameters
-scales = [i for i = 1:25]
+scales = [i for i = 1:50]
 
 Γ = 0.2
 
@@ -109,10 +115,10 @@ cost_s = fill(1, 5) * 5
 # cost_s = rand(1:5, 5) ./ 10
 
 file_name = "./Experiments/Set $(Dates.today()) $(Dates.format(now(), "HH MM SS")).txt"
-file_name = "./Experiments/Relaxed Set $(Dates.today()) $(Dates.format(now(), "HH MM SS")).txt"
+# file_name = "./Experiments/Relaxed Set $(Dates.today()) $(Dates.format(now(), "HH MM SS")).txt"
 
-A = create_matrix(-10:10, 5, 5, zero_diag=false)
-println("$A")
+# A = create_matrix(-10:10, 5, 5, zero_diag=false)
+# println("$A")
 
 run_experiments(A, Γ, cost_r, cost_s, file_name=file_name, scales=scales, relax=false)
 
