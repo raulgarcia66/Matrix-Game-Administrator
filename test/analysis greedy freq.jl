bef30c34752afea0cc2bd15ad90d35eb5b63131c
Raul Garcia <rauljgarcia66@gmail.com>
2023-08-28 14:46:53 -0500

    Upd

--- test/analysis greedy freq.jl
+++ test/analysis greedy freq.jl
@@ -107,10 +107,10 @@ master_dict["MILP"]
 master_dict["Greedy with s"]
 master_dict["Greedy with dual var"]
 
-##### Analysis merged DF
+##### Analysis merged DF (MILP vs one Greedy freq ranking)
 df_temp_MILP = deepcopy(master_dict["MILP"])
-df_temp_greedy, g_suffix = deepcopy(master_dict["Greedy with s"]), "s"
-# df_temp_greedy, g_suffix = deepcopy(master_dict["Greedy with dual var"]), "dual"
+# df_temp_greedy, g_suffix = deepcopy(master_dict["Greedy with s"]), "s"
+df_temp_greedy, g_suffix = deepcopy(master_dict["Greedy with dual var"]), "dual"
 select!(df_temp_MILP, Not(["Matrix seed", "Costs seed", "Node count"]))
 select!(df_temp_greedy, Not(["Matrix seed", "Costs seed"]))
 
@@ -137,12 +137,51 @@ df_merged[:,"Obj val_MILP - Obj val_$g_suffix"] = df_merged[:,"Obj val_MILP"] - 
 df_merged[:,"Budget spent smaller_MILP"] = df_merged[:,"Budget spent_MILP"] .< df_merged[:,"Budget spent_$g_suffix"]
 # TODO: Add columns that count how many optimal rows and cols the Greedy approach purchased
 
-minimum(df_merged[:,"Obj val_MILP - Obj val_$g_suffix"])  # should be very small
+minimum(df_merged[:,"Obj val_MILP - Obj val_$g_suffix"])
 
 gdf = groupby(df_merged, ["c_s range", "Budget fraction", "Term status_MILP"])
 
-gdf_agg = combine(gdf, "Obj val larger_MILP" => count, "Budget spent smaller_MILP" => count,
+gdf_agg = combine(gdf, nrow => "Group size", "Obj val larger_MILP" => count, "Budget spent smaller_MILP" => count,
         "Solve time_MILP" => mean, "Rel gap_MILP"
         )
 
 gdf[("11:15", 0.5, "TIME_LIMIT")][:,"Obj val_MILP - Obj val_$g_suffix"]
+
+
+##### Analysis merged DF (Greedy freq s vs Greedy freq dual)
+# df_temp_MILP = deepcopy(master_dict["MILP"])
+df_temp_greedy_s = deepcopy(master_dict["Greedy with s"])
+df_temp_greedy_dual = deepcopy(master_dict["Greedy with dual var"])
+select!(df_temp_greedy_s, Not(["Matrix seed", "Costs seed"]))
+select!(df_temp_greedy_dual, Not(["Matrix seed", "Costs seed"]))
+
+df_merged = deepcopy(df_temp_greedy_s)
+rename!(df_merged, "Obj val" => "Obj val_s", "Budget spent" => "Budget spent_s",
+        "Rows purchased" => "Rows purchased_s", "Cols purchased" => "Cols purchased_s",
+        )
+
+# Add cols from greedy freq
+df_merged[:,"Obj val_dual"] = df_temp_greedy_dual[!,"Obj val"]
+df_merged[:,"Budget spent_dual"] = df_temp_greedy_dual[!,"Budget spent"]
+df_merged[:,"Rows purchased_dual"] = df_temp_greedy_dual[!,"Rows purchased"]
+df_merged[:,"Cols purchased_dual"] = df_temp_greedy_dual[!,"Cols purchased"]
+#### Compute comparisons
+df_merged[:,"Obj val larger_dual"] = df_merged[:,"Obj val_dual"] .> df_merged[:,"Obj val_s"]
+df_merged[:,"Obj val larger_s"] = df_merged[:,"Obj val_dual"] .< df_merged[:,"Obj val_s"]
+df_merged[:,"Obj vals equal"] = df_merged[:,"Obj val_dual"] .== df_merged[:,"Obj val_s"]
+df_merged[:,"Obj val_dual - Obj val_s"] = df_merged[:,"Obj val_dual"] - df_merged[:,"Obj val_s"]
+
+# TODO: Normalize difference in obj values
+# TODO: Add columns that count how rows and cols each Greedy freq approach purchased that the other didn'table
+
+# TODO: Careful with analysis among different matrix sizes (e.g., solve time average)
+gdf = groupby(df_merged, ["c_s range", "Budget fraction"])
+
+gdf_agg = combine(gdf, nrow => "Group size", "Obj val larger_dual" => count => "Dual obj larger", "Obj val larger_s" => count => "s obj larger",
+        "Obj vals equal" => count => "Obj vals equal", "Obj val_dual - Obj val_s" => mean, "Budget spent_dual" => mean, "Budget spent_s" => mean,
+        "Rows purchased_dual" => mean, "Rows purchased_s" => mean, "Cols purchased_dual" => mean, "Cols purchased_s" => mean,
+        )
+
+gdf_agg[:,1:7]
+gdf_agg[:,8:end]
+
